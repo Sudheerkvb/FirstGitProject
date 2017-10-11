78f2db47844209cc05c56122b8d4a8113812fd6c
sudheer <kvbsudheer@gmail.com>
2017-10-11 08:24:49 +0530

    newlocalchnages

---
+++ src/com/git/example/HelloWorld.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.git.example;
+
+/**
+ * @author sudheer
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+ 
+		System.out.println(" Hi ");
+		System.out.println(" Hi ");
+		System.out.println(" Hi ");
+
+	}
+
+}
+
